acdd9229bbc378b19a35dac0c70ec85e8b79ede7
Sam Qing <samallenqing@gmail.com>
2020-03-31 22:04:03 -0500

    Add header and collection components.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './menu-item.styles.scss'
+import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) =>(
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
 
-    <div className={`${size} menu-item`}>
-        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}} />
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>Shop Now</span>
@@ -12,4 +13,4 @@ const MenuItem = ({title, imageUrl, size}) =>(
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
